dda2c35567fc045365ecb16b58b6d10c2ec81dfc
Elies AMARELO <eamarelo@hotmail.fr>
2020-07-09 03:15:29 -0700

    first commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+	//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by drei-mac on 08/07/2020.
+//  Copyright © 2020 mds.netbrutconverter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Test!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
